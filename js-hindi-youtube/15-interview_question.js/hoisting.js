be72d8abf4667f2e468a7e94ef695f6140b0c6e0
Ajit kumar <74586437+Ajitkumarkrimuddichak@users.noreply.github.com>
2025-11-18 08:00:26 +0530

    Refactor hoisting.js comments for clarity
    Updated comments to clarify hoisting behavior in JavaScript.

--- js-hindi-youtube/15-interview_question.js/hoisting.js
+++ js-hindi-youtube/15-interview_question.js/hoisting.js
@@ -1,33 +1,65 @@
-/** What is Hoisting in js :- A javascript mechanism where variables with 'var' and 'function'
-declarations are moved to the top of scope before code execution.
+/** 
+What is Hoisting in js :- Hoisting is JavaScript's behaviour of moving declarations to the top of their scope
+during the compilation before the code actually runs.
+- Only declarations are hoisted, not initializations.
+
 => function declarations are properly hoisted (not arrow function).
-=> var is hoisted.
+=> var is hoisted. But let, const are not hoisted.
 **/
 
 //Ex:-
-// myPlace = "Patna"; //var is hoisted
-// console.log(myPlace);
-// var myPlace; // Variable Declaration
+/*
+myPlace = "Patna"; // var is hoisted. Veriable value is Hoisted only for.
+console.log(myPlace);
+var myPlace; // Variable Declaration
+*/
 
-//(or)
-// let myName = "Ajit";
-// sayHi(); //valid calling
-
-// function sayHi() {
-//   let lastName = "kumar";
-//   console.log(myName, lastName);
-// }
-
-// sayHello(); //Not valid calling Error
-// let sayHello = () => {
-//   console.log(myName);
-// };
-
-//(or) Hamesa Declaration hoisted hote hai
-console.log(a);
-greet();
-function greet() {
-  console.log("Good Morning");
+//(or) function Declaration
+/*
+function sayHi() {
+  var Name;
+  console.log(Name); // undefined because declaration hoisted but initialization is not
+  Name = "Kumar";
 }
-var a = 10; // Declaration hoisted to the top but initialization is not
-console.log(a);
+sayHi();
+*/
+
+// (or) This function name is function Declaration. and Hoisting is Possible.
+/*
+sayHi();
+function sayHi() {
+  console.log(Name); // undefined because declaration hoisted but initialization is not
+  var Name = "Kumar";
+}
+*/
+
+// (or) function Declaration
+/*
+sayHi();
+function sayHi() {
+  Name = "Kumar";
+  console.log(Name); // Kumar because initialization hoisted also
+  var Name;
+}
+*/
+
+//Ex:- Arrow function and function expression are not hoisted.
+/*
+sayHello(); //Not valid calling Error
+let sayHello = () => {
+  var myName;
+  console.log(myName);
+  myName = "Ajit";
+};
+*/
+
+//(or) function expression
+/*
+sayHello(); //Not valid calling Error
+// Yaha pe Dekho to pura function one veriable me store kiya hai. yeha pe function ko initialization kiya hai.
+var sayHello = function () {
+  var myName;
+  console.log(myName);
+  myName = "Ajit";
+};
+*/
